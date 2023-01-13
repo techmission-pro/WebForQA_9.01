2bc4a7c10c39fbd4880c1be8adc0f7702955429e
YuriyP77 <19yurikp77@gmail.com>
2023-01-13 12:15:16 +0200

    Added calculator 1; 2.

---
+++ resources/calc.js
@@ -0,0 +1,21 @@
+document.querySelector('.calculate').addEventListener('click', function () {
+	const num1 = parseFloat(document.querySelector('[name="num1"]'). value);
+	const num2 = parseFloat(document.querySelector('[name="num2"]'). value);
+	const operation = document.querySelector('[name="operation"]'). value;
+	
+	let result;
+	if (operation === 'plus') {
+		result = num1 + num2;
+	} else if (operation === 'minus') {
+		result = num1 - num2;
+	} else if (operation === 'multiply') {
+		result = num1 * num2;
+	} else if (operation === 'divide') {
+		result = num1 / num2;
+	} else {
+		console.error('Wrong operation');
+		alert('Wrong operation is selected');
+	}
+	
+	document.querySelector('.calc-result').innerText = result;
+});
