02613dde141beb1fb5a9ee0f297fdabc7f7dd8d3
SerhiiDolgoruk <dolgorswb@gmail.com>
2023-01-23 17:40:17 +0200

    correction click

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,8 @@
-document.querySelector('.calc-calculate').addEventListener('clic', function () {
+document.querySelector('.calc-calculate').addEventListener('click', function () {
     const num1 = +document.querySelector('[name="num1"]').value;	
 	const num2 = +document.querySelector('[name="num2"]').value;	
 	
 	const sun = num1 + num2;
 	
-	document.querySelector('calc-result').value = sun;
+	document.querySelector('.calc-result').value = sun;
 });                                                                                      
