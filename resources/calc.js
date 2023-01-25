3a3a9ba11885705fbb3ce95f1187582ec180f95e
Gennadii76 <kgeshe@gmail.com>
2023-01-25 14:25:39 +0200

    calc-calculate

--- resources/calc.js
+++ resources/calc.js
@@ -1,10 +1,8 @@
-document.querySelector('.calc-calculate').addEventListener('click',function () {
+document.querySelector('.calc-calculate').addEventListener('click', function() {
 	const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
 	
 	const sum = num1 + num2;
 	
-	document.querySelector('.calc-result').innerText = sum;
+	document.querySelector('.calc-result').value = sum;
 });
-
- 
