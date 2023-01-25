e0e39ef9561223236d6df4438a04f8c6504b52fd
Oleksii-Sorokhtej <a.sorohtej@gmail.com>
2023-01-25 21:15:05 +0200

    add calc (step 2)

--- resources/calc.js
+++ resources/calc.js
@@ -1,6 +1,16 @@
 document.querySelector ('.calc-button-result').addEventListener('click', function () {
 const num1 = Number(document.querySelector ('[name="num1"]').value);
 const num2 = +document.querySelector ('[name="num2"]').value;
-const sum = num1 + num2;
-document.querySelector('.calc-result').value = sum;
+const operation=document.getElementById("operation").value;
+let result;
+if (operation == "plus"){
+    result = num1 + num2;
+}else if (operation == "minus"){
+    result = num1 - num2;
+}else if (operation == "dobutok"){
+    result = num1 * num2;
+}else if(operation == "chastka"){
+    result = num1 / num2;
+}
+document.querySelector('.calc-result').value = result;
 } );
