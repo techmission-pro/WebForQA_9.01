db4134ff89022735646441c60fcb2c081ea8523c
oksanabilenko <mot.oksanka@gmail.com>
2023-01-25 21:08:35 +0200

    calc

--- resources/calc.js
+++ resources/calc.js
@@ -1,5 +1,5 @@
 console.log('calc-loaded');
-document.querySelector ('.calc-calculate').addEventListener('click',Function() {
+document.querySelector ('.calc-calculate').addEventListener('click',function() {
 	const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
 	const sum= num1+num2;
