162a5741e7c87c1e6a5affde559d832af835aecd
Artem Stetsiuk <a.stetsiuk1@gmail.com>
2023-01-27 21:55:04 +0200

    new repositoru

---
+++ resources/calc.js
@@ -0,0 +1,40 @@
+function calculete() {
+    const num1 = +document.querySelector('[name="num1"]').value;
+    const num2 = +document.querySelector('[name="num2"]').value;
+    const op = document.querySelector('[name="op"]').value;
+
+    let result;
+
+    if (op === 'plus') {
+        result = num1 + num2;
+    } else if (op === 'minus') {
+        result = num1 - num2;
+    } else if (op === 'multiply') {
+        result = num1 * num2;
+    } else if (op === 'divide') {
+        result = num1 / num2
+    } else {
+        console.error('Wrong op');
+        alert('Wrong operation is selected');
+    }
+
+    document.querySelector('.calc-result').value = result;
+}
+
+
+
+document.querySelector('.calc-calculete').addEventListener('click', function() {
+    calculete();
+});
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+    calculete();
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+    calculete();
+});
+
+document.querySelector('[name="num2 "]').addEventListener('input', function() {
+    calculete();
+});
