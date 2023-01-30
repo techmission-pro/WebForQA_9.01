cc132b20c85d8b582ef7c4e659a693d85ba6fb36
Oleksii-Sorokhtej <a.sorohtej@gmail.com>
2023-01-30 20:22:55 +0200

    autocalc

--- resources/calc.js
+++ resources/calc.js
@@ -1,16 +1,38 @@
-document.querySelector ('.calc-button-result').addEventListener('click', function () {
-const num1 = Number(document.querySelector ('[name="num1"]').value);
-const num2 = +document.querySelector ('[name="num2"]').value;
-const operation=document.getElementById("operation").value;
-let result;
-if (operation == "plus"){
-    result = num1 + num2;
-}else if (operation == "minus"){
-    result = num1 - num2;
-}else if (operation == "dobutok"){
-    result = num1 * num2;
-}else if(operation == "chastka"){
-    result = num1 / num2;
-}
-document.querySelector('.calc-result').value = result;
-} );
+ function calculate() { 
+    const num1 = +document.querySelector('[name="num1"]').value;
+    const num2 = +document.querySelector('[name="num2"]').value;
+    const op = document.querySelector('[name="op"]').value;
+    
+     let result;
+     
+         if (op === 'plus') {
+             result = num1 + num2;
+         } else if (op === 'minus') {
+             result = num1 - num2;
+         } else if (op === 'multiply') {
+             result = num1 * num2;
+         } else if (op === 'divide') {
+             result = num1 / num2;
+         } else {
+         console.error('Wrong op!');
+         alert('Wrong operation is selected');
+         }
+     
+    document.querySelector('.calc-result').value=result;
+     }
+     
+     document.querySelector('.calc-calculate').addEventListener('click',function() {
+         calculate();
+     });
+     
+     document.querySelector('[name="op"]').addEventListener('change',function() {
+         calculate();
+     });
+             
+     document.querySelector('[name="num1"]').addEventListener('input',function() {
+         calculate();
+     });
+     
+     document.querySelector('[name="num2"]').addEventListener('input',function() {
+         calculate();
+     });
