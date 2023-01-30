3a2568ce2ad956eb788e68eae0874af89c72c8b1
Svitlyk <vanjasvitlik14@gmail.com>
2023-01-30 22:23:11 +0200

    add calculate"op"

--- resources/calc.js
+++ resources/calc.js
@@ -1,6 +1,34 @@
-document.querySelector('.calc-calculate').addEventListener('click', function () {
+ function calculate () {
 const nam1 = + document.querySelector('[name="nam1"]').value;
 const nam2 = + document.querySelector('[name="nam2"]').value;
- const sum = nam1 + nam2;
- document.querySelector ('.calc-result').value = sum;
+ const op =  document.querySelector('[name="op"]').value;
+ let result;
+ if (op=== 'plus') {
+	 result=nam1 +nam2;
+ } else if (op=== 'minus') {
+ result=nam1 - nam2;}
+	 else if (op=== 'multiple') {
+	 result=nam1 * nam2;}
+	 else if (op=== 'divide') {
+	 result=nam1 / nam2;}
+	 else    {console.error('Wrong op');
+alert('Wrong operation is selected ');}
+	 
+
+ document.querySelector ('.calc-result').value = result;
+}
+
+
+
+document.querySelector('.calc-calculate').addEventListener('click',function(){ calculate();
 });
+
+ document.querySelector('[name="op"]').addEventListener('change',function()
+ { calculate ();
+ });
+  document.querySelector('[name="nam1"]').addEventListener('input',function()
+ { calculate ();
+ });
+  document.querySelector('[name="nam2"]').addEventListener('input',function()
+ { calculate ();
+ });
